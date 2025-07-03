64bff8652381cb49f0176ac253a94f899ddc4e53
Elías <eliashz@gmail.com>
2025-07-03 11:37:26 +0200

    Nav bar links

--- app/components/PublicNavBar.tsx
+++ app/components/PublicNavBar.tsx
@@ -1,3 +1,4 @@
+import { SignIn, SignInButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,6 +13,20 @@ export default function PublicNavBar() {
                     alt="Calendar logo"
                 />
             </Link>
+            <section className="sticky top-0 flex justify-between">
+                <div className="flex flex-1 max-sm:gap-0 sm:gap-6">
+                    <SignInButton>
+                        <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 cursor-pointer rounded-xl shadow-2xl">
+                            Login
+                        </Button>
+                    </SignInButton>
+                    <SignUpButton>
+                        <Button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 cursor-pointer rounded-xl shadow-2xl" variant={"ountline"}>
+                            Register
+                        </Button>
+                    </SignUpButton>
+                </div>
+            </section>
         </nav>
     )
 }
